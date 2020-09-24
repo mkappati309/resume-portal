cc812bf988326ea99ee17b09eed334e3d80e1ade
mkappati309 <52052642+mkappati309@users.noreply.github.com>
2020-09-24 10:44:43 -0500

    Changed package name of main application and the controller package..

--- resume-portal/src/main/java/com/javabrains/resumeportal/controller/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.javabrains.resumeportal.controller;
--- resume-portal/src/main/java/io/javabrains/resumeportal/app/ResumePortalApplication.java
+++ resume-portal/src/main/java/io/javabrains/resumeportal/app/ResumePortalApplication.java
@@ -1,4 +1,4 @@
-package io.javabrains.resumeportal;
+package io.javabrains.resumeportal.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
